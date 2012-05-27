f625f50c5063bfbb2fb77b6e16ea1a098fb0ee51
mantera <som4th@gmail.com>
2012-05-27 14:48:03 -0400

    Recovery: Always show the "Go Back" option and Power key selects options.
    Always show the Go Back option on the menus except for the main menu.
    The Power key is set to Select items.
    
    Change-Id: I8a9f99c944bb4c1c6477515b6617454752d7d8ae

--- default_recovery_keys.c
+++ default_recovery_keys.c
@@ -20,43 +20,15 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
 int device_handle_key(int key_code, int visible) {
     if (visible) {
         switch (key_code) {
-            case KEY_CAPSLOCK:
-            case KEY_DOWN:
             case KEY_VOLUMEDOWN:
-            case KEY_MENU:
                 return HIGHLIGHT_DOWN;
 
-            case KEY_LEFTSHIFT:
-            case KEY_UP:
             case KEY_VOLUMEUP:
-            case KEY_HOME:
                 return HIGHLIGHT_UP;
 
             case KEY_POWER:
-                if (ui_get_showing_back_button()) {
-                    return SELECT_ITEM;
-                }
-                if (!get_allow_toggle_display() && ui_menu_level > 0) {
-                    return GO_BACK;
-                }
-                break;
-            case KEY_LEFTBRACE:
-            case KEY_ENTER:
-            case BTN_MOUSE:
-            case KEY_CAMERA:
-            case KEY_F21:
-            case KEY_SEND:
                 return SELECT_ITEM;
             
-            case KEY_END:
-            case KEY_BACKSPACE:
-            case KEY_SEARCH:
-                if (ui_get_showing_back_button()) {
-                    return SELECT_ITEM;
-                }
-                if (!get_allow_toggle_display() && ui_menu_level > 0) {
-                    return GO_BACK;
-                }
             case KEY_BACK:
                 if (ui_menu_level > 0) {
                     return GO_BACK;
